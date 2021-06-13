6f5dcce568d2df0bea461075135835e270016f79
mpmcgee <mpmcgee@iu.edu>
2021-06-12 21:51:25 -0400

    added dot product and menu

--- src/processor/Main.java
+++ src/processor/Main.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 public class Main {
 
 
-    public static int[][] createMatrix(){
+    public static double[][] createMatrix(){
         Scanner scanner = new Scanner(System.in);
         String dims = scanner.nextLine();
         String[] arrDims = dims.split(" ");
         int numRows = Integer.parseInt(arrDims[0]);
         int numCols = Integer.parseInt(arrDims[1]);
 
-        int[][] matrix = new int[numRows][numCols];
+        double[][] matrix = new double[numRows][numCols];
 
         for (int row = 0; row < numRows; row++) {
             for (int col = 0; col < numCols; col++) {
-                matrix[row][col] = scanner.nextInt();
+                matrix[row][col] = scanner.nextDouble();
             }
         }
 
@@ -23,9 +23,13 @@ public class Main {
 
     }
 
-    public static void addMatrices(int[][] matrixA, int[][] matrixB) {
+    public static void addMatrices() {
+
+        double[][] matrixA = createMatrix();
+        double[][] matrixB = createMatrix();
+
         if (matrixA.length == matrixB.length && matrixA[0].length == matrixB[0].length) {
-            int[][] sumMatrix = new int[matrixA.length][matrixA[0].length];
+            double[][] sumMatrix = new double[matrixA.length][matrixA[0].length];
 
             for (int row = 0; row < sumMatrix.length; row++) {
                 for (int col = 0; col < sumMatrix[0].length; col++) {
@@ -40,13 +44,14 @@ public class Main {
             }
 
 
+
         } else {
             System.out.println("ERROR");
         }
     }
 
     public static void multiplyMatrixByConstant() {
-        int[][] matrix = createMatrix();
+        double[][] matrix = createMatrix();
 
         Scanner scanner = new Scanner(System.in);
         int cons = scanner.nextInt();
@@ -65,13 +70,81 @@ public class Main {
 
     }
 
-    public static void main(String[] args) {
+    public static double multiplyMatrixCells(double[][] matrixA, double[][] matrixB, int row, int col) {
+        double cell = 0;
 
-        // int[][] matrixA = createMatrix();
-        // int[][] matrixB = createMatrix();
+        for (int i = 0; i < matrixB.length; i++) {
+            cell += matrixA[row][i] * matrixB[i][col];
+        }
+
+        return cell;
+    }
+
+    public static void multiplyMatrices() {
+        double[][] matrixA = createMatrix();
+        double[][] matrixB = createMatrix();
+        double[][] productMatrix = new double[matrixA.length][matrixB[0].length];
+        double product = 0;
+
+        if (matrixA[0].length == matrixB.length) {
+            for (int row = 0; row < productMatrix.length; row++) {
+                for (int col = 0; col < productMatrix[0].length; col++) {
+                    productMatrix[row][col] = multiplyMatrixCells(matrixA, matrixB, row, col);
+                }
+            }
+
+            for (int row = 0; row < productMatrix.length; row++) {
+                for (int col = 0; col < productMatrix[0].length; col++) {
+
+                    if (col == 0) {
+                        System.out.print("\n");
+                    }
+
+                    System.out.print(productMatrix[row][col] + " ");
+                }
+            }
+        } else {
+            System.out.println("ERROR");
+        }
+    }
+
+    public static void printMenu() {
+        System.out.println("\n");
+        System.out.println("1. Add matrices\n" +
+                "2. Multiply matrix by a constant\n" +
+                "3. Multiply matrices\n" +
+                "0. Exit\n" +
+                "Your choice: ");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
 
         // addMatrices(matrixA, matrixB);
 
-        multiplyMatrixByConstant();
+        // multiplyMatrixByConstant();
+
+        int selection = 6;
+
+        while (selection != 0) {
+            printMenu();
+            selection = scanner.nextInt();
+            switch (selection) {
+                case 1:
+                    addMatrices();
+                    break;
+                case 2:
+                    multiplyMatrixByConstant();
+                    break;
+                case 3:
+                    multiplyMatrices();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Invalid selection.");
+            }
+        }
     }
 }
